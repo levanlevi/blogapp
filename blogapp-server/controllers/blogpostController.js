dc43ddd4841a6d8b33278c35caa75b6c73c0ac9c
Levan Khunjgurua <l.khunjgurua@gmail.com>
2017-06-20 22:52:55 +0200

    BlogPost controller clean up

--- blogapp-server/controllers/blogpostController.js
+++ blogapp-server/controllers/blogpostController.js
@@ -27,9 +27,61 @@ var blogpostController = function (Blogpost) {
         });
     }
 
+
+    // Middleware attached blogpost object. 
+    // It is defined in blogspotRoutes.js
+    var getById = function (req, res) {
+        res.send(req.blogpost);
+    };
+
+    /**
+     * Replaces object with fields provided in request
+     */
+    var putById = function (req, res) {
+        req.blogpost.title = req.body.title;
+        req.blogpost.body = req.body.body;
+        req.blogpost.author = req.body.author;
+        req.blogpost.date = req.body.date;
+        req.blogpost.categories = req.body.categories;
+        req.blogpost.save();
+        res.json(req.blogpost);
+    }
+    /**
+     * Updates by id. Maintains field value not included in request
+     */
+    var patchById = function (req, res) {
+        if (req.body._id)
+            delete req.body._id;
+
+        for (var p in req.body) {
+            req.blogpost[p] = req.body[p];
+        }
+
+        req.blogpost.save(function (err) {
+            if (err)
+                res.status(500).send(err);
+            else
+                res.json(req.blogpost);
+        })
+    }
+
+    var deleteById = function (req, res) {
+        req.blogpost.remove(function (err) {
+            if (err)
+                res.status(500).send();
+            else
+                res.status(204).send('Removed');
+        });
+    }
+
+
     return {
         post: post,
-        get: get
+        get: get,
+        getById: getById,
+        putById: putById,
+        patchById: patchById,
+        deleteById: deleteById
     };
 
 };
--- blogapp-server/routes/blogpostRoutes.js
+++ blogapp-server/routes/blogpostRoutes.js
@@ -23,42 +23,10 @@ var routes = function (Blogpost) {
     });
 
     blogpostRouter.route('/:blogpostId')
-        .get(function (req, res) {
-            res.send(req.blogpost);
-        })
-        .put(function (req, res) { // replace with id
-            req.blogpost.title = req.body.title;
-            req.blogpost.body = req.body.body;
-            req.blogpost.author = req.body.author;
-            req.blogpost.date = req.body.date;
-            req.blogpost.categories = req.body.categories;
-            req.blogpost.save();
-            res.json(req.blogpost);
-        })
-        .patch(function (req, res) { // update with id. maintains field value not included in request
-            if (req.body._id)
-                delete req.body._id;
-
-            for (var p in req.body) {
-                req.blogpost[p] = req.body[p];
-            }
-
-            req.blogpost.save(function (err) {
-                if (err)
-                    res.status(500).send(err);
-                else
-                    res.json(req.blogpost);
-            })
-        })
-        .delete(function (req, res) {
-            req.blogpost.remove(function (err) {
-                if (err)
-                    res.status(500).send();
-                else
-                    res.status(204).send('Removed');
-            });
-        });
-
+        .get(blogpostController.getById)
+        .put(blogpostController.putById)
+        .patch(blogpostController.patchById)
+        .delete(blogpostController.deleteById);
 
     return blogpostRouter;
 };
