0308f9f9aee0b65d888417383e98193f74b0357f
Levan Khunjgurua <l.khunjgurua@gmail.com>
2017-06-20 18:25:02 +0200

    Authentication separated

--- blogapp-server/routes/secureRoutes.js
+++ blogapp-server/routes/secureRoutes.js
@@ -3,6 +3,41 @@ var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 
 var routes = function (app) {
     var secureRouter = express.Router();
+    var User = require('./../models/userModel'); 
+    // route to authenticate a user (POST http://localhost:8080/api/authenticate)
+    app.post('/authenticate', function (req, res) {
+        // find the user
+        User.findOne({
+            name: req.body.name
+        }, function (err, user) {
+
+            if (err) throw err;
+
+            if (!user) {
+                res.json({ success: false, message: 'Authentication failed. User not found.' });
+            } else if (user) {
+
+                // check if password matches
+                if (user.password != req.body.password) {
+                    res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+                } else {
+
+                    // if user is found and password is right
+                    // create a token
+                    var token = jwt.sign(user, app.get('superSecret'), {});
+                    //{ expiresInMinutes: 1440 // expires in 24 hours }
+
+
+                    // return the information including token as JSON
+                    res.json({
+                        success: true,
+                        message: 'Enjoy your token!',
+                        token: token
+                    });
+                }
+            }
+        });
+    });
 
     secureRouter.use('/', function (req, res, next) {
         // check header or url parameters or post parameters for token
